4b741b6c0bd6bcab6a90cd58e901ef0702791bd1
libitha james <libithajames@gmail.com>
2020-12-29 16:53:00 +0530

    Hello:

---
+++ datatypeok.java
@@ -0,0 +1,15 @@
+class datatypeok{
+       public static void main(String args[]){
+       int a=120;
+       System.out.println("byte size="+Byte.SIZE+"bits");
+       System.out.println("byte Max value: "+Byte.MAX_VALUE);
+       System.out.println("byte Min value: "+Byte.MIN_VALUE);
+       System.out.println("int size="+Integer.SIZE+"bits");
+       System.out.println("int Max value:  "+Integer.MAX_VALUE);
+       System.out.println("int Min value:  "+Integer.MIN_VALUE);
+    
+
+
+
+}
+}   
